673a9012ca74a7083d2db7cb53d8e8bef3bdc0fd
igordreher <igord.sparrenberger@gmail.com>
2021-04-20 14:44:08 -0300

    Add Player component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import { Header } from '../components/Header';
+import { Player } from '../components/Player';
+import styles from '../styles/app.module.scss';
 import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
-      <Header />
-      <Component {...pageProps} />;
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />;
+      </main>
+      <Player />
     </div>
   );
 }
