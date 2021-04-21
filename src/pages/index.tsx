b290727b4ade5a16cafd2ec97874c8bbbfc26e83
igordreher <igord.sparrenberger@gmail.com>
2021-04-21 13:32:14 -0300

    Update data formating from api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,54 @@
-export default function Home(props) {
+import { GetStaticProps } from 'next';
+import { format, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import api from '../services/api';
+import convertDurationToTimeString from '../utils/convertDurationToTimeString';
+
+type Episode = {
+  id: string,
+  title: string,
+  members: string,
+  publishedAt: string,
+  thumbnail: string,
+  description: string,
+  url: string,
+  duration: number,
+  durationAsString: string;
+};
+
+type HomeProps = {
+  episodes: Array<Episode>;
+};
+
+export default function Home(props: HomeProps) {
   return (
-    <h1>Index</h1>
+    <div>{JSON.stringify(props.episodes)}</div>
   );
 }
 
-export async function getStaticProps() {
-  const response = await fetch('http://localhost:3333/episodes');
-  const data = await response.json();
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  });
+
+  const episodes = data.map((episode) => {
+    const { file, published_at, ...new_episode } = episode;
+    return {
+      ...new_episode, url: episode.file.url,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR }),
+      duration: episode.file.duration,
+      durationAsString: convertDurationToTimeString(episode.file.duration)
+    };
+  });
 
   return {
     props: {
-      episodes: data
+      episodes
     },
     revalidate: 60 * 60 * 8
   };
-}
+};
